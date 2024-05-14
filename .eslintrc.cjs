899ec5b0c1dc9b43e28e278468178f3a7e5ebd8e
ammarhussien <ammar.hussienmo97@gmail.com>
2024-05-14 21:45:26 +0300

    first step

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,7 +4,7 @@ module.exports = {
   extends: [
     "react-app",
     //"eslint:recommended",
-    "plugin:react/recommended",
+    //"plugin:react/recommended",
     "plugin:react/jsx-runtime",
     "plugin:react-hooks/recommended",
   ],
